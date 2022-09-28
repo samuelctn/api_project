a642d17ea762229a887ef83f1af0c5f509c4bb3d
Samuel Constantin <samysamuel000@gmail.com>
2022-09-28 21:11:19 +0300

    Delete apikey.py

--- apikey.py
+++
@@ -1,5 +0,0 @@
-key = '8aef8a54-7ebd-4495-b682-c905e56d9547'
-twitter_key = '0m09xJxqcNTFOMF0tzzwNDovB'
-
-secret_key_tw = 'IVBVqDxrbyEfKTzKxlO2HGuejN2nxvhRQ213MTSKXnYXh253cT'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAADw8hgEAAAAAEe6h9Ip5qoDIBVJoFDnFP%2FVqWlM%3D0jI5fdMkMCmWPAlORyQtJgQ9QBYJVkmkpoR62bzuqw4YHHvDku'
